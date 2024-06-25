b5f6f5453d5003c0c35e368d5008963d6210dc3c
Mrinank Raj <mrinankrajsingh67@gmail.com>
2024-06-26 03:07:38 +0530

    Completed excercises 1-7.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(x,y) {
+    if(x<0 || y<0){
+        return "ERROR"
+    }
+    if(typeof(x)!=="number" || typeof(y)!=="number"){
+        return "ERROR"
+    }
+    let sum=0
+    if(y>x){
+        for(let i=x;i<=y;i+=1){
+            sum+=i
+        }
+    }
+    else if(x>y){
+        for(let i=x;i>=y;i-=1){
+            sum+=i
+        }
+    }
+    return sum
 };
 
 // Do not edit below this line
